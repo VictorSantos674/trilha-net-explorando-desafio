9bd46ae598ca4a0937cb4acea4766835e3464fa7
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-09-05 19:26:01 +0000

    feat: create Suite class with type, capacity and daily rate

--- Models/Suite.cs
+++ Models/Suite.cs
@@ -2,6 +2,10 @@ namespace DesafioProjetoHospedagem.Models
 {
     public class Suite
     {
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+        
         public Suite() { }
 
         public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
@@ -10,9 +14,5 @@ namespace DesafioProjetoHospedagem.Models
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
         }
-
-        public string TipoSuite { get; set; }
-        public int Capacidade { get; set; }
-        public decimal ValorDiaria { get; set; }
     }
 }
